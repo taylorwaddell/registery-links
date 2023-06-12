db549bd48957f7d0a27c6a289ebd4e0219c93c8e
Taylor Waddell <74875060+taylorwaddell@users.noreply.github.com>
2023-06-11 21:55:42 -0500

    Foundation laid.

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,138 @@
-import Image from 'next/image'
+import LinkCardListItem from "@/public/Components/LinkCardListItem";
+
+interface LinkCards {
+  id: number;
+  column: number;
+  link: string;
+  title: string;
+  summary: string;
+}
 
 export default function Home() {
+  const linkCards: LinkCards[] = [
+    {
+      id: 1,
+      column: 1,
+      link: "https://www.google.com/maps/dir//Shaw+Center+for+the+Arts,+100+Lafayette+St,+Baton+Rouge,+LA+70801/@30.4479022,-91.1915779,17z/data=!3m1!5s0x8626a0c14b427007:0x2626a4a9e03c0a6!4m9!4m8!1m0!1m5!1m1!1s0x8626a0c6b6401ad5:0x14a34040c17b1959!2m2!1d-91.189003!2d30.4478976!3e0?entry=ttu",
+      title: "Directions to Reception",
+      summary: "Remember your dance shoes.",
+    },
+    {
+      id: 2,
+      column: 1,
+      link: "https://www.taylorwaddell.com",
+      title: "Photo Gallery",
+      summary: "Come back later to see our wedding pictures.",
+    },
+    {
+      id: 3,
+      column: 1,
+      link: "https://www.google.com",
+      title: "Google",
+      summary: "Learn somethin'.",
+    },
+    {
+      id: 4,
+      column: 2,
+      link: "#",
+      title: "Cash Fund",
+      summary: "Honey moon and other fun things",
+    },
+    {
+      id: 5,
+      column: 2,
+      link: "#",
+      title: "Amazon",
+      summary: "A short description. Maybe even a longer one. I don't care.",
+    },
+    {
+      id: 6,
+      column: 2,
+      link: "#",
+      title: "Target",
+      summary: "A short description.",
+    },
+    {
+      id: 7,
+      column: 3,
+      link: "#",
+      title: "RSVP: Reception",
+      summary: "Let's party 🎉",
+    },
+  ];
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
+    <main className="mx-auto mt-6 mb-12 w-10/12 sm:w-5/6 lg:w-3/4 xl:w-1/2">
+      <h1 className="w-full text-center text-2xl">T A L Y</h1>
+      <div className="py-8 px-6 border-2 border-zinc-300 dark:border-zinc-700 rounded-md shadow-sm my-10">
+        <p className="mb-5">
+          Welcome to our wedding information website, where love and celebration
+          come together! We are thrilled to have you join us on this joyous
+          occasion and extend our warmest welcome to all our guests.
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        <p>
+          Here, you will find a treasure trove of details about our upcoming
+          wedding, ensuring that you have all the information you need to fully
+          enjoy and participate in this memorable event.
+        </p>
+      </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-8 mt-10">
+        <div className="space-y-2">
+          <h2 className="font-light text-lg pl-2 text-zinc-600 dark:text-zinc-500">
+            Wedding Info
+          </h2>
+          <ul className="space-y-2">
+            {linkCards.map((linkCard) => {
+              if (linkCard.column !== 1) return;
+              return (
+                <LinkCardListItem
+                  key={linkCard.id}
+                  title={linkCard.title}
+                  link={linkCard.link}
+                  summary={linkCard.summary}
+                />
+              );
+            })}
+          </ul>
+        </div>
+        <div className="space-y-2">
+          <h2 className="font-light text-lg pl-2 text-zinc-600 dark:text-zinc-500">
+            Registries
+          </h2>
+          <ul className="space-y-2">
+            {linkCards.map((linkCard) => {
+              if (linkCard.column !== 2) return;
+              return (
+                <LinkCardListItem
+                  key={linkCard.id}
+                  title={linkCard.title}
+                  link={linkCard.link}
+                  summary={linkCard.summary}
+                />
+              );
+            })}
+          </ul>
+        </div>
+        <div className="space-y-2">
+          <h2 className="font-light text-lg pl-2 text-zinc-600 dark:text-zinc-500">
+            Misc
+          </h2>
+          <ul className="space-y-2">
+            {linkCards.map((linkCard) => {
+              if (linkCard.column !== 3) return;
+              return (
+                <LinkCardListItem
+                  key={linkCard.id}
+                  title={linkCard.title}
+                  link={linkCard.link}
+                  summary={linkCard.summary}
+                />
+              );
+            })}
+          </ul>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
---
+++ public/Components/LinkCardListItem.tsx
@@ -0,0 +1,19 @@
+import ExternalLink from "../icons/ExternalLink";
+
+interface Props {
+  link: string;
+  title: string;
+  summary: string;
+}
+
+export default function LinkCardListItem({ title, link, summary }: Props) {
+  return (
+    <li className="p-2">
+      <a href={link} className="flex font-semibold hover:underline">
+        {title}
+        <ExternalLink className={"ml-1"} />
+      </a>
+      <p className="text-sm text-zinc-500 dark:text-zinc-500">{summary}</p>
+    </li>
+  );
+}
