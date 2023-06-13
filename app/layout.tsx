c9b441119cee9617431a5b4dacc5f7741fb5b744
Taylor Waddell <74875060+taylorwaddell@users.noreply.github.com>
2023-06-12 22:50:02 -0500

    Countdown implemented.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 
+import Countdown from "@/public/Components/Countdown";
 import { Inter } from "next/font/google";
 import Link from "next/link";
 
@@ -21,10 +22,19 @@ export default function RootLayout({
         className={`min-h-screen flex flex-col ${inter.className} bg-zinc-200 dark:bg-zinc-800 text-zinc-900 dark:text-zinc-200`}
       >
         {children}
-        <footer className="border-t border-zinc-700 w-full text-center p-4 text-zinc-500 dark:text-zinc-500">
+        <footer className="flex justify-center border-t border-zinc-700 w-full text-center p-4 text-zinc-500 dark:text-zinc-500 text-sm md:text-base">
           <p>
-            made with ♥ by <Link className="hover:underline" href="https://twadd.dev" target="_blank">twadd</Link>
+            made with ♥ by{" "}
+            <Link
+              className="hover:underline"
+              href="https://twadd.dev"
+              target="_blank"
+            >
+              twadd
+            </Link>
           </p>
+          <span className="px-5">|</span>
+          <Countdown targetDate="2023-11-10" />
         </footer>
       </body>
     </html>
---
+++ public/Components/Countdown.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+// Countdown.tsx
+import { useEffect, useState } from "react";
+
+interface CountdownProps {
+  targetDate: string;
+}
+
+const Countdown: React.FC<CountdownProps> = ({ targetDate }) => {
+  const calculateTimeLeft = () => {
+    const difference = +new Date(targetDate) - +new Date();
+
+    let timeLeft = {
+      weeks: 0,
+      days: 0,
+      hours: 0,
+    };
+
+    if (difference > 0) {
+      timeLeft = {
+        weeks: Math.floor(difference / (1000 * 60 * 60 * 24 * 7)),
+        days: Math.floor((difference / (1000 * 60 * 60 * 24)) % 7),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      };
+    }
+
+    return timeLeft;
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  });
+
+  return (
+    <p>
+      {timeLeft.weeks} Weeks {timeLeft.hours} Hours {timeLeft.days} Days
+    </p>
+  );
+};
+
+export default Countdown;
