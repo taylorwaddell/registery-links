4a6c82e103306cee452f66c7a43bcdd96f77c873
taylorwaddell <Taylor.Waddell@verisglobal.com>
2023-06-24 00:40:39 -0500

    Improved Homepage.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Countdown from "@/public/Components/Countdown";
+import ExternalLink from "@/public/icons/ExternalLink";
 import { FormEvent } from "react";
 import Link from "next/link";
 
@@ -13,8 +13,38 @@ export default function Home() {
   return (
     <>
       <main className="flex-grow mx-auto mt-6 mb-12 w-10/12 sm:w-5/6 lg:w-3/4 xl:w-1/2">
-        Welcome to Wedding Info!
-        <form
+        <div className=" mb-8">
+          <h1 className="text-2xl">Welcome to Wedding Info!</h1>
+        </div>
+        <div className="flex flex-col gap-2">
+          <p className="flex">
+            <Link
+              className="flex font-semibold underline hover:underline-offset-2"
+              href="#"
+              target="_blank"
+            >
+              Sign Up
+            </Link>
+            &nbsp;or&nbsp;
+            <Link
+              className="flex font-semibold underline hover:underline-offset-2"
+              href="/login"
+              target="_blank"
+            >
+              Sign In
+            </Link>
+            &nbsp;to create a Wedding Info page.
+          </p>
+          <Link
+            className="flex font-semibold underline hover:underline-offset-2"
+            target="_blank"
+            href="/i/6b72fa05-e22f-4cba-b494-34b334e74159"
+          >
+            See an example.
+            <ExternalLink className="ml-1" />
+          </Link>
+        </div>
+        {/* <form
           className="flex gap-1"
           action="submit"
           method="get"
@@ -31,7 +61,7 @@ export default function Home() {
           >
             Search
           </button>
-        </form>
+        </form> */}
       </main>
       <footer className="flex justify-center border-t border-zinc-700 w-full text-center p-4 text-zinc-500 dark:text-zinc-500 text-sm md:text-base">
         <p>
